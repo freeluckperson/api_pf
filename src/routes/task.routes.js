171877ac611ce857c1730333b6ca53e076e5e89f
freeluckperson <gestionecesaria@gmail.com>
2023-08-05 22:52:55 -0400

    video min 1:40:00

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 const router = Router()
 import { authRequired } from '../middlewares/validateToken.js'
+import { getTasks, getTask, updateTask, createTask, deleteTask } from '../controllers/tasks.controller.js'
 
-router.get('/task', authRequired, (req, res)=>{
-    res.send('task function')
-})
+router.get('/task', authRequired, getTasks)
+router.get('/task/:id', authRequired, getTask)
+router.put('/task/:id', authRequired, updateTask)
+router.post('/task', authRequired, createTask)
+router.delete('/task/:id', authRequired, deleteTask)
+
 
 export default router
